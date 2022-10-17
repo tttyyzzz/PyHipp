bb4236b18fec5223eb99347fb544d90ba4a4fc20
AWS ParallelCluster user <ec2-user@ip-10-0-0-243.ap-southeast-1.compute.internal>
2022-10-17 04:02:30 +0000

    add personal bash scripts

---
+++ mycheckfiles2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Number of hkl files"
+find . -name "*.hkl" | grep -v -e spiketrain -e mountains | wc -l
+
+echo "Number of mda files"
+find mountains -name "firings.mda" | wc -l
+
+echo "Time taken (s)"
+tail pipe-slurm*.out
+
+echo "Start Times"
+head -n 1 *.out
+
+echo "End Times"
+tail -n -5 *.out
